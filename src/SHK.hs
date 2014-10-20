e590132872cf4e55fa23d9b30af2802f9efd7e47
Tobias Axell <tobias.axell@gmail.com>
2014-10-20 12:45:15 +0200

    Fixed problem with keywords in strings

--- src/SHK.hs
+++ src/SHK.hs
@@ -19,8 +19,28 @@ toJavaFileExt fp = (dropExtension fp) ++ ".java"
 isJSHSource :: FilePath -> Bool
 isJSHSource = isSuffixOf sourceExt
 
-toJava :: String -> String
-toJava p = unlines $ map (unwords . map translate . words) $ lines $ lex p
+--toJava :: String -> String
+toJava p = let lexed = lines $ lex p
+               split = map (collectAnnotations . annotateStrings) lexed
+           in unlines $ concat $ map (\l -> map (\(s, b) -> if b then s else (unwords (map translate (words s) ))) l) split
+
+
+-- unlines $ map (unwords . map translate . words) $ lines $ lex p
+
+collectAnnotations :: [(Char, Bool)] -> [(String, Bool)]
+collectAnnotations []              = []
+collectAnnotations ((c, b) : rest) = 
+    ( (c : (map fst (takeWhile (\x -> snd x == b) rest))) , b) : collectAnnotations (dropWhile (\x -> snd x == b) rest)
+
+annotateStrings :: String -> [(Char, Bool)]
+annotateStrings = as' False
+    where
+        as' :: Bool -> String -> [(Char, Bool)]
+        as' _ []                       = []
+        as' b  ('\\' : '\"' : rest)    = ('\\', b) : ('\"', b) : as' b rest
+        as' True  ('\"' : rest)        = ('\"', True) : as' False rest
+        as' False ('\"' : rest)        = ('\"', True) : as' True rest
+        as' b (c : rest)               = (c, b) : as' b rest
 
 sourceExt :: String
 sourceExt = ".jsh" 
---
+++ test_jsh_src/StringWithKeywords.jsh
@@ -0,0 +1,6 @@
+offentlig klass StringWithKeywords {
+
+    offentlig fast tomhet main(String[] args){
+        System.out.println(" denna hel bryt ");
+    }
+}
